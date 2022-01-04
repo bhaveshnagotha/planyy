98ec5c2e7c00e2f109954350b4d3825aeac05dec
Bhavesh <bhavesh.nagotha@gmail.com>
2022-01-04 22:56:35 +0530

    04012022-hm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/common/header/header.component';
+import { FooterComponent } from './pages/common/footer/footer.component';
+import { HomeComponent } from './pages/features/home/home.component';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReadsComponent } from './pages/features/reads/reads.component';
+//import { LayoutRoutingModule } from 'src/app/pages/layout/layout-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ReadsComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //LayoutRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/features/home/home.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    var self = this
+
+    // var swiper = new Swiper(".mySwiper", {
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //   },
+    // });
+
+
+    function checkForInput(element: any) {
+      // element is passed to the function ^
+
+      const $label = $(element).siblings(".demo-label");
+
+      // if ($(element).val().length > 0) {
+      //   $label.addClass("input-has-value");
+      // } else {
+      //   $label.removeClass("input-has-value");
+      // }
+    }
+
+    // The lines below are executed on page load
+    $("input.textdemo").each(function () {
+      checkForInput(self);
+    });
+
+    // The lines below (inside) are executed on change & keyup
+    $("input.textdemo").on("change keyup", function () {
+      checkForInput(self);
+    });
+
+
+    if (window.matchMedia("(max-width: 1023px)").matches) {
+      $(document).ready(function () {
+        $(".planyybody").removeClass("sidebaractive");
+
+        setTimeout(function () {
+          $(".planyybody").addClass("sidebaractive1");
+        }, 1);
+      });
+    } else if (window.matchMedia("(max-width: 1023px)").matches) {
+      $(document).ready(function () {
+        $(".planyybody").removeClass("sidebaractive");
+
+        setTimeout(function () {
+          $(".planyybody").addClass("sidebaractive2");
+        }, 1);
+      });
+    } else {
+      $(document).ready(function () {
+        $(".planyybody").removeClass("sidebaractive");
+
+        setTimeout(function () {
+          $(".planyybody").addClass("sidebaractive");
+        }, 2000);
+      });
+    }
+
+
+
+
+
+
+    function openOverlay() {
+      $("#dark-overlay").removeClass("d-none");
+    }
+
+    function closeOverlay() {
+      $("#dark-overlay").addClass("d-none");
+    }
+
+    $(".custom-modal .close").click(function () {
+      $("#dark-overlay").addClass("d-none");
+    });
+
+    $(".close").click(function () {
+      $("#dark-overlay-beneath").addClass("d-none");
+      $(".close").parent().addClass("d-none");
+    });
+
+    $("#right-menu-login .close,#right-menu .close").click(function () {
+      $("#dark-overlay-beneath").addClass("d-none");
+      $(self).parent().addClass("d-none");
+    });
+
+    $(".slider_text_cont_p1 [src='./assets/custom/images/message.svg']").click(
+      function () {
+        openOverlay();
+        $("#message-modal").removeClass("d-none");
+      }
+    );
+
+    $(".slider_text_cont_p1 [src='./assets/custom/images/call.svg']").click(function () {
+      openOverlay();
+      $("#call-modal").removeClass("d-none");
+    });
+
+    $("[src='./assets/custom/images/user.svg").click(function () {
+      openOverlay();
+      $("#signup-modal").removeClass("d-none");
+    });
+
+    $("[data-target='#exampleModal6']").click(function () {
+      $("#dark-overlay-beneath").removeClass("d-none");
+      $("#right-menu").removeClass("d-none");
+    });
+
+    $(".timepicker").timepicki({ reset: true });
+
+    $("#signup-btn").click(function () {
+      openOverlay();
+      $("#signup-modal").addClass("d-none");
+      $("#otp-modal").removeClass("d-none");
+    });
+
+  }
+
+}
